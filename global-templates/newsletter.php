f9b06d0ac00acf4f2f301dde4055f4a0f9d81dad
sebastianferrari_onlines <sebastian.ferrari@onlines.com.ar>
2020-10-07 17:42:54 -0300

    Mailchimp integration

--- global-templates/newsletter.php
+++ global-templates/newsletter.php
@@ -16,13 +16,16 @@
 
 			<div class="block__content">
 
-				<form action="#" method="POST" class="form form--footer">
+				<form method="POST" class="form form--footer" action="https://voto.us10.list-manage.com/subscribe/post" method="post" target="popupwindow" onsubmit="window.open('https://voto.us10.list-manage.com/subscribe/post', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
 					<div class="form__grid">
+						
+						<input type="hidden" name="u" value="5fd1872fc9e6862c76e6066ef">
+						<input type="hidden" name="id" value="09d33d31d3">
 
 						<div class="input">
 							<label class="input__label" for="input-name"><?php echo pll__('Nome'); ?></label>
 							<div class="input__box">
-								<input name="data-name" id="input-name" type="text">
+								<input name="MERGE1" id="input-name" type="text" required>
 							</div>
 							<!--/input-box-->
 						</div>
@@ -31,7 +34,8 @@
 						<div class="input">
 							<label class="input__label" for="input-email"><?php echo pll__('E-mail'); ?></label>
 							<div class="input__box">
-								<input name="data-email" id="input-email" type="email">
+								<input type="hidden" value="1" name="embed"/>
+								<input name="MERGE0" id="input-email" type="email" required>
 							</div>
 							<!-- input-box-->
 						</div>
