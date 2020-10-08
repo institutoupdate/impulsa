7571a0ed3724a168d8492f19881d768a58f3c973
Miguel Peixe <miguel@peixe.co>
2020-10-08 13:02:30 -0300

    add url support for home slider

--- global-templates/home/slider.php
+++ global-templates/home/slider.php
@@ -5,20 +5,31 @@
         <div class="swiper-container js-slider">
 
             <div class="swiper-wrapper">
-                <?php while( have_rows('slider-home') ) : the_row(); 
-                    $slider_home_title = get_sub_field('slider-home_title'); 
-                    $slider_home_img = get_sub_field('slider-home_img'); 
+                <?php while( have_rows('slider-home') ) : the_row();
+                    $slider_home_title = get_sub_field('slider-home_title');
+                    $slider_home_img = get_sub_field('slider-home_img');
+                    $slider_home_url = get_sub_field('slider-home_url');
                 ?>
                 <div class="swiper-slide">
                     <div class="slide">
-                        <?php if($slider_home_img) { 
+                        <?php if($slider_home_img) {
                             $slider_home_img_url = $slider_home_img['url'];
                         ?>
                         <div class="slide__bg" style="background-image:url(<?php echo $slider_home_img_url; ?>);"></div>
                         <?php } ?>
                         <?php if($slider_home_title) { ?>
                         <div class="container">
-                            <h3 class="slide__title"><?php echo $slider_home_title; ?></h3>
+                            <h3 class="slide__title">
+                              <?php if($slider_home_url) : ?>
+                                <a href="<?php echo $slider_home_url; ?>">
+                              <?php endif; ?>
+
+                              <?php echo $slider_home_title; ?>
+
+                              <?php if($slider_home_url) : ?>
+                                </a>
+                              <?php endif; ?>
+                            </h3>
                         </div>
                         <!--/container-->
                         <?php } ?>
@@ -31,7 +42,7 @@
             <!-- swiper-wrapper -->
 
             <div class="swiper-pagination"></div>
-            
+
         </div>
         <!--/swiper-container-->
 
