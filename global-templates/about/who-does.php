e5693502034a4a1431b4795e2cacf52e77aa5b08
Miguel Peixe <miguel@peixe.co>
2020-10-09 13:43:14 -0300

    rearrange about page meta fields

--- global-templates/about/who-does.php
+++ global-templates/about/who-does.php
@@ -1,5 +1,7 @@
 <?php
 $about_who_does_title = get_field('about-who-does_title');
+$about_who_does_support_title = get_field('about-who-does_support-title');
+$about_who_does_support_text = get_field('about-who-does_support-text');
 ?>
 <div class="semiblock semiblock--m-bottom-xlg">
 
@@ -7,10 +9,12 @@ $about_who_does_title = get_field('about-who-does_title');
     <h3 class="title-2 title-2--sm title-2--c-1 title-2--m-bottom-md"><?php echo $about_who_does_title; ?></h3>
     <?php } ?>
 
-    <?php if( have_rows('about-who-does_items') ):
+    <?php if( have_rows('about-who-does_items') ): ?>
+    <div class="box box--m-bottom-xlg">
+    <?php
     $i = 1;
     $rowCount = count( get_field('about-who-does_items') );
-    while( have_rows('about-who-does_items') ) : the_row(); 
+    while( have_rows('about-who-does_items') ) : the_row();
         $about_who_does_list_title = get_sub_field('about-who-does_items_title');
         $about_who_does_list_content = get_sub_field('about-who-does_items_content');
         $about_who_does_list_logo = get_sub_field('about-who-does_items_logo');
@@ -29,7 +33,7 @@ $about_who_does_title = get_field('about-who-does_title');
             </div>
             <?php } ?>
 
-            <?php if($about_who_does_list_logo) { 
+            <?php if($about_who_does_list_logo) {
                 $about_who_does_list_logo_url = $about_who_does_list_logo['url'];
             ?>
             <img class="img-logo img-logo--sm" src="<?php echo $about_who_does_list_logo_url; ?>" alt="<?php echo $about_who_does_list_title; ?>">
@@ -40,7 +44,28 @@ $about_who_does_title = get_field('about-who-does_title');
 
     </div>
     <!--/box-->
-    <?php $i++; endwhile; else : endif; ?>
-    
+    <?php $i++; endwhile; ?>
+    </div>
+    <?php endif; ?>
+
+    <div class="box">
+
+        <?php if($about_who_does_support_title) { ?>
+        <h4 class="title-3 title-3--uppercase title-3--m-bottom"><?php echo $about_who_does_support_title; ?></h4>
+        <?php } ?>
+
+        <?php if($about_who_does_support_text) { ?>
+        <div class="grid grid--6y2">
+            <div class="text">
+                <?php echo $about_who_does_support_text; ?>
+            </div>
+            <!--/text-->
+        </div>
+        <!--/grid-6y2-->
+        <?php } ?>
+
+    </div>
+    <!--/box-->
+
 </div>
 <!--/semiblock-->
