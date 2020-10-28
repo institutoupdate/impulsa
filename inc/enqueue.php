d42d83fa5e35c97edeb9c5247c65bdc72a44841a
Miguel Peixe <miguel@peixe.co>
2020-10-28 14:58:46 -0300

    update styles

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -9,7 +9,7 @@ if ( ! function_exists( 'enqueue_scripts' ) ) {
 
 		//$the_theme = wp_get_theme();
 		// Get the custom theme data.
-		wp_enqueue_style( 'styles', get_stylesheet_directory_uri() . '/css/screen.css', array(), '2.1' );
+		wp_enqueue_style( 'styles', get_stylesheet_directory_uri() . '/css/screen.css', array(), '2.1.2' );
 		wp_enqueue_style( 'webfonts' , 'https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:ital,wght@0,300;0,700;1,300&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap', array(), false);
 
 		//wp_enqueue_script( 'jquery-last', get_template_directory_uri() . '/vendor/js/jquery-3.2.1.min.js', array(), '', true);
