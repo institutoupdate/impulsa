ce9be28709076253334f925c7ae1348ff1057c2f
Miguel Peixe <miguel@peixe.co>
2021-03-25 11:19:54 -0300

    change redirect url

--- single-tracks.php
+++ single-tracks.php
@@ -1,5 +1,5 @@
-<?php 
-//if (have_posts()) : while (have_posts()) : the_post(); 
+<?php
+//if (have_posts()) : while (have_posts()) : the_post();
 
 // Track ID
 $track_id = get_the_ID();
@@ -9,9 +9,9 @@ $track_posts = get_field('track_relationship');
 if($track_posts) {
     // First ID
     $track_post_id = $track_posts['0']->ID;
-    
+
     // Redirect to material/post
-    wp_redirect( get_home_url().'/?p='.$track_post_id.'&track='.$track_id );
+    wp_redirect( get_permalink($track_post_id) . '?track='.$track_id );
     exit;
 } else {
     // Redirect home
