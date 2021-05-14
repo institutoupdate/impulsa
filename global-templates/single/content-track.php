03e3bbee700c87ade599b5ca149edd2e87eba43a
Miguel Peixe <miguel@peixe.co>
2021-05-14 12:30:54 -0300

    Do not display featured image on track single content

--- global-templates/single/content-track.php
+++ global-templates/single/content-track.php
@@ -7,12 +7,6 @@
 
     <h2 class="title-4 title-4--lg title-4--m-bottom-xs"><?php the_title(); ?></h2>
 
-    <?php if(has_post_thumbnail()) { 
-        $thumb = get_the_post_thumbnail_url(get_the_ID(), 'large');
-    ?>
-        <div class="article__img article__img-m-bottom-sm"><div class="article__bg" style="background-image:url(<?php echo $thumb; ?>);"></div></div>
-    <?php } ?>
-
     <span class="article__date"><?php echo $date; ?></span>
 
     <div class="entry entry--m-top entry--m-bottom-md">
